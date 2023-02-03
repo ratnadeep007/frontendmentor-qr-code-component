c7029eccd46d1dfcd810da3902de17e2fb555411
Ratnadeep Bhattacharyya <ratnadeepofghato@gmail.com>
2023-02-04 02:42:13 +0530

    init

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://ratnadeep007.github.io',
+  base: '/frontendmentor-qr-code-component',
+  integrations: [tailwind()]
+});
